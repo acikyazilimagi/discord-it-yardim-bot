6dff0afb446c597156a8db3113295c06d79c5475
Arda Barış Tonç <62848063+abtonc@users.noreply.github.com>
2023-02-08 16:55:37 +0300

    Revert "eslint, prettier and pre-commit hook setup"

--- commands/rol.js
+++ commands/rol.js
@@ -1,12 +1,11 @@
-const { SlashCommandBuilder } = require('discord.js')
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('rol')
-    .setDescription('Nasıl rol alınabileceği ve kayıt olabileceği hakkında bilgi içerir.'),
-  async execute(interaction) {
-    await interaction.reply(
-      'Çok kısa sürede kocaman bir ekip olduk, proje yönetiminin sağlıklı bir şekilde yapılabilmesi için yeni gelen arkadaşlarımızın aşağıdaki kayıt formunu doldurması gerekmektedir. Formu doldurduktan sonra ilgili ekiplerimiz başvurunuzu inceleyerek ihtiyaç halinde sizinle iletişime geçeceklerdir. O zaman kadar duyuru kanallarımızdan tüm projelerimiz hakkında bilgi alabilirsin.\n[Form linki için tıklayın!](<https://app.retable.io/form/j3DtfkIkYlqj2cxn>)',
-    )
-  },
-}
+	data: new SlashCommandBuilder()
+		.setName('rol')
+		.setDescription('Nasıl rol alınabileceği ve kayıt olabileceği hakkında bilgi içerir.'),
+	async execute(interaction) {
+		await interaction.reply('Çok kısa sürede kocaman bir ekip olduk, proje yönetiminin sağlıklı bir şekilde yapılabilmesi için yeni gelen arkadaşlarımızın aşağıdaki kayıt formunu doldurması gerekmektedir. Formu doldurduktan sonra ilgili ekiplerimiz başvurunuzu inceleyerek ihtiyaç halinde sizinle iletişime geçeceklerdir. O zaman kadar duyuru kanallarımızdan tüm projelerimiz hakkında bilgi alabilirsin.\n[Form linki için tıklayın!](<https://app.retable.io/form/j3DtfkIkYlqj2cxn>)');
+	},
+};
+
