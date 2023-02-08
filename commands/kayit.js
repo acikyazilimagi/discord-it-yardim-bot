99d11492a25e625ec55aef86529a199a897986de
Çağatay Karahan <kodbilenadam@gmail.com>
2023-02-09 02:45:55 +0300

    Kayıt.js eklendi
    Kayıt komutu eklendi
    Kullanım şekli: /kayıt Ad + Soyad

---
+++ commands/kayit.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require('discord.js')
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('kayıt')
+    .setDescription('Ad ve soyad değiştirmenizi sağlar.')
+    .addStringOption((option) => option.setName('ad').setDescription('Adınızı giriniz.').setRequired(true))
+    .addStringOption((option) => option.setName('soyad').setDescription('Soyadınızı giriniz.').setRequired(true)),
+
+  async execute(interaction) {
+    try {
+      const ad = interaction.options.getString('ad')
+      const soyad = interaction.options.getString('soyad')
+      await interaction.member.setNickname(`${ad} ${soyad}`)
+      await interaction.reply('Ad ve soyadınız değiştirildi.')
+    } catch (error) {
+      console.error(error)
+      console.log(error.rawError)
+      console.log(error.rawError.message)
+      await interaction.reply(
+        `Ad ve soyadınız değiştirilemedi. Hata: ${error.rawError.code} - ${error.rawError.message}`,
+      )
+    }
+  },
+}
