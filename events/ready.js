6f70bb0c1f5292e9ec1c02123dac4acf95b21547
Fiobrum <70747825+Fiobrum@users.noreply.github.com>
2023-02-09 21:47:39 +0300

    Saat başı hatırlatma eklenmesi
    Discord'a yeni katılan insanlara bilgilendirme geçilmesi adına paylaşılan metnin kendi botumuza uyarlanmış versiyonunu hazırladım.

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,26 @@
-const { Events } = require('discord.js');
-
+const { Events, EmbedBuilder  } = require('discord.js');
+const an_channel = require('../data/config.json')
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		//SAAT BAŞI HATIRLATMA 
+		setInterval(function mesaj() {
+			const an_Embed = new EmbedBuilder()
+			.setColor(000000)
+			.setTitle('Herkese merhabalar, hoşgeldiniz! 🙌🏻')
+			.addFields(
+				{ name: 'Çok kısa sürede kocaman bir ekip olduk, Lütfen kayıt formunu doldurun ilgili ekiplerimiz başvurunuzu inceleyerek ihtiyaç halinde sizinle iletişime geçeceklerdir. O zaman kadar duyuru kanallarımızdan tüm projelerimiz hakkında bilgi alabilirsin.', value: "[📌 Kayıt Formu](https://app.retable.io/form/j3DtfkIkYlqj2cxn)"},
+				{ name: 'Fikirlerinizi bizimle paylaşmaya hevesli olduğunuz için çok mutluyuz.Fakat fikirlerinizin incelenmesi ve hiç bir fikrin gözden kaçmaması için aşağıdaki formu doldurmanız gerekmektedir.', value: "[📌 Fikir Formu](https://app.retable.io/form/RVC67K3Ai5kQ5w4s)" },
+				{ name: 'Geliştirilen uygulamalar ve sunucu ile ilgili feedbacklerinizi aşağıdaki formu doldurarak bize iletebilirsiniz.', value: "[📌 Feedback Formu](https://app.retable.io/form/NIfo34pdWuweWtfI)" },
+				{ name: 'İlginiz için teşekkürler sizi daha yakından tanımamız için aşağıdaki formu lütfen doldurun. Kurumsal iletişim ekibimiz sizler ile iletişime geçeceklerdir.', value: "[📌 Kurumsal Destek Formu](https://app.retable.io/form/qdjgyl2NHrE5CLnA)" },
+			)
+			.setTimestamp()
+			.setFooter({ text: 'Her bir saatte bir otomatik paylaşılır.'})
+
+			client.channels.cache.get(an_channel.main_Announcment_channel).send({ embeds: [an_Embed] });
+		}, 3600 * 1000);
+		//SAAT BAŞI HATIRLATMA 
 	},
 };
