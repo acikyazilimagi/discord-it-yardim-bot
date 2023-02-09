f63ef18f20d547b4519a88bca7ce42d799f0a1be
BLOK <hi@ilkem.space>
2023-02-09 05:45:14 +0300

    fix(cmd): komutlarin tepkileri pingleyecek sekilde degistirildi

--- commands/bireysel-destek.js
+++ commands/bireysel-destek.js
@@ -1,14 +1,12 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js')
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('bireysel-destek')
-    .setDescription(
-      'Bize nasıl bireysel destek verebileceğiniz hakkında bilgi içerir.'
-    ),
+    .setDescription('Bize nasıl bireysel destek verebileceğiniz hakkında bilgi içerir.'),
   async execute(interaction) {
     await interaction.reply(
-      `Merhaba, ${interaction.user.id} eğer formu dolduysanız size ulaşabiliriz.\nŞuan insan kaynağı dolmuş durumdadır.\n[Form linki için tıklayın!](<https://app.retable.io/form/qdjgyl2NHrE5CLnA>)`
-    );
+      `Merhaba, <@${interaction.user.id}> eğer formu dolduysanız size ulaşabiliriz.\nŞuan insan kaynağı dolmuş durumdadır.\n[Form linki için tıklayın!](<https://app.retable.io/form/qdjgyl2NHrE5CLnA>)`,
+    )
   },
-};
+}
--- commands/feedback.js
+++ commands/feedback.js
@@ -1,14 +1,12 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js')
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('feedback')
-    .setDescription(
-      'Feedbacklerinizi bizimle nasıl paylaşabileceğiniz hakkında bilgi içerir.'
-    ),
+    .setDescription('Feedbacklerinizi bizimle nasıl paylaşabileceğiniz hakkında bilgi içerir.'),
   async execute(interaction) {
     await interaction.reply(
-      `${interaction.user.id}, Geliştirilen uygulamalar ve sunucu ile ilgili feedbacklerinizi aşağıdaki formu doldurarak bize iletebilirsiniz.\n[Form linki için tıklayın!](<https://app.retable.io/form/NIfo34pdWuweWtfI>)`
-    );
+      `<@${interaction.user.id}>, Geliştirilen uygulamalar ve sunucu ile ilgili feedbacklerinizi aşağıdaki formu doldurarak bize iletebilirsiniz.\n[Form linki için tıklayın!](<https://app.retable.io/form/NIfo34pdWuweWtfI>)`,
+    )
   },
-};
+}
