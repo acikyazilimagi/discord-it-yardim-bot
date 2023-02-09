15ef5736aa03e68ad420a2bf666fa58d20d5c6f5
BLOK <31442227+itsBLOK@users.noreply.github.com>
2023-02-09 04:51:16 +0300

    Update kayit.js
    reply sadece kisinin gorecegi sekilde degistirildi
    await interaction.reply({ content: '*******', ephemeral: true }); yaparak sadece kisinin gormesini saglayabilirsiniz

--- commands/kayit.js
+++ commands/kayit.js
@@ -12,14 +12,13 @@ module.exports = {
       const ad = interaction.options.getString('ad')
       const soyad = interaction.options.getString('soyad')
       await interaction.member.setNickname(`${ad} ${soyad}`)
-      await interaction.reply('Ad ve soyadınız değiştirildi.', { ephemeral: true })
-      await interaction.deleteReply()
+      await interaction.reply({ content: 'Ad ve soyadınız değiştirildi.', ephemeral: true })
     } catch (error) {
       console.error(error)
       // console.log(error.rawError)
       // console.log(error.rawError.message)
       await interaction.reply(
-        `Ad ve soyadınız değiştirilemedi. Hata: ${error.rawError.code} - ${error.rawError.message}`,
+        { content: `Ad ve soyadınız değiştirilemedi. Hata: ${error.rawError.code} - ${error.rawError.message}`, ephemeral: true },
       )
     }
   },
