37368df7299229ad8bea48394ef270a934a1a538
Ramazan SANCAR <ramazansancar4545@gmail.com>
2023-02-09 17:56:00 +0300

    + "komutlar.js" updated.

--- commands/komutlar.js
+++ commands/komutlar.js
@@ -4,7 +4,7 @@ module.exports = {
   data: new SlashCommandBuilder().setName('komutlar').setDescription('Sunucudaki komutlar hakkında bilgi verir.'),
   async execute(interaction) {
     await interaction.reply({
-      content: `${interaction.user}, \`/komutlar:\` IT Deprem Yardım tarafından geliştirilen projelerimizi görüntüleyebilir ve haklarında bilgi alabilirsiniz.\n\`/rol:\` Nasıl rol alınabileceği ve kayıt olabileceği hakkında bilgi içerir.\n\`/fikir:\` Fikirlerinizi bizimle nasıl paylaşabileceğiniz hakkında bilgi içerir.\n\`/feedback:\`  Feedbacklerinizi bizimle nasıl paylaşabileceğiniz hakkında bilgi içerir.\n\`/kurumsal-destek:\` Bize nasıl kurumsal destek verebileceğiniz hakkında bilgi içerir.`,
+      content: `${interaction.user},\n\`/komutlar:\` IT Deprem Yardım tarafından geliştirilen projelerimizi görüntüleyebilir ve haklarında bilgi alabilirsiniz.\n\`/rol:\` Nasıl rol alınabileceği ve kayıt olabileceği hakkında bilgi içerir.\n\`/fikir:\` Fikirlerinizi bizimle nasıl paylaşabileceğiniz hakkında bilgi içerir.\n\`/feedback:\`  Feedbacklerinizi bizimle nasıl paylaşabileceğiniz hakkında bilgi içerir.\n\`/kurumsal-destek:\` Bize nasıl kurumsal destek verebileceğiniz hakkında bilgi içerir.`,
       ephemeral: true,
     })
   },
