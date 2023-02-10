4a76d7cf86407710e15c8af8e6560e7d25ce3c02
Ramazan SANCAR <ramazansancar4545@gmail.com>
2023-02-10 22:11:43 +0300

    + try catch yapısı eklendi.

--- commands/translate.js
+++ commands/translate.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js')
+const translate = require('translatte')
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -8,11 +9,28 @@ module.exports = {
     .addStringOption((option) => option.setName('language').setDescription('Language to translate').setRequired(true)),
 
   async execute(interaction) {
-    const word = interaction.options.getString('word')
-    const language = interaction.options.getString('language')
-    const translate = require('translatte')
-    const result = await translate(word, { to: language.toUpperCase })
-    console.log(interaction)
-    await interaction.reply(`<@${interaction.user.id}> :  ${result.text}`)
+    try {
+      const word = interaction.options.getString('word')
+      const language = interaction.options.getString('language')
+      const result = await translate(word, { to: language.toUpperCase() })
+      console.log('reuslt:', result)
+      await interaction.reply(
+        `${interaction.user} :  [${result.from.language.iso.toUpperCase()}]->[${language.toUpperCase()}] ${
+          result.text
+        }`,
+      )
+    } catch (error) {
+      console.error(error)
+      console.log(error.rawError)
+      console.log(error.rawError.message)
+      // rawError: { message: 'Unknown interaction', code: 10062 },
+      if (error.rawError) {
+        await interaction.reply(
+          `Bir hata oluştu. Lütfen daha sonra tekrar deneyiniz. **Hata:** ${error.rawError.message} (${error.rawError.code})`,
+        )
+      } else {
+        await interaction.reply(`Bir hata oluştu. Lütfen daha sonra tekrar deneyiniz. **Hata:** ${error.message}`)
+      }
+    }
   },
 }
