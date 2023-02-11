d0445af6ed54a5313b7d087c5ed0ca79c02e2747
Mehmet Berkay Özbay <berkayozbay64@gmail.com>
2023-02-11 19:06:40 +0300

    Announcement interval change depends on the time

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 const { Events, EmbedBuilder } = require('discord.js')
 const { mainAnnouncement } = require('../data/config.json')
 
@@ -8,22 +9,45 @@ module.exports = {
     console.log(`Ready! Logged in as ${client.user.tag}`)
     // SAAT BAŞI HATIRLATMA
     setInterval(() => {
-      const announcementEmbed = new EmbedBuilder()
-        .setColor(0xf26666)
-        .setTitle('Herkese merhabalar, hoşgeldiniz! 🙌🏻')
-        .setDescription(
-          `Çok kısa sürede kocaman bir ekip olduk, proje yönetiminin sağlıklı bir şekilde yapılabilmesi için yeni gelen arkadaşlarımızın aşağıdaki kayıt formunu doldurması gerekmektedir. Formu doldurduktan sonra ilgili ekiplerimiz başvurunuzu inceleyerek ihtiyaç halinde sizinle iletişime geçeceklerdir. O zaman kadar duyuru kanallarımızdan tüm projelerimiz hakkında bilgi alabilirsin.\n**[📌 Kayıt Formu](https://app.retable.io/form/j3DtfkIkYlqj2cxn)**\n\nFikirlerinizi bizimle paylaşmaya hevesli olduğunuz için çok mutluyuz.Fakat fikirlerinizin incelenmesi ve hiç bir fikrin gözden kaçmaması için aşağıdaki formu doldurmanız gerekmektedir.\n**[📌 Fikir Formu](https://app.retable.io/form/RVC67K3Ai5kQ5w4s)** | **[🗣 Github Discussions](<https://github.com/acikkaynak/deprem-yardim-projesi/discussions>)**\n\nGeliştirilen uygulamalar ve sunucu ile ilgili feedbacklerinizi aşağıdaki formu doldurarak bize iletebilirsiniz.\n **[📌 Feedback Formu](https://app.retable.io/form/NIfo34pdWuweWtfI)**\n\nİlginiz için teşekkürler sizi daha yakından tanımamız için aşağıdaki formu lütfen doldurun. Kurumsal iletişim ekibimiz sizler ile iletişime geçeceklerdir.\n**[📌 Kurumsal Destek Formu](https://app.retable.io/form/qdjgyl2NHrE5CLnA)**`,
-        )
-        .addFields({
-          name: '**Açık Yazılım Ağı**',
-          value:
-            '**[⛓ Github Repoları](https://github.com/orgs/acikkaynak/repositories)\n[⛓ Instagram](https://www.instagram.com/acikyazilimagi/)\n[⛓ Twitter](https://twitter.com/acikyazilimagi)**',
-        })
-        .setTimestamp()
-        .setFooter({ text: 'Her bir saatte bir otomatik paylaşılır.' })
+      // if time is not between 9am and 9pm
+      if (new Date().getHours() >= 9 && new Date().getHours() <= 21) {
+        const announcementEmbed = new EmbedBuilder()
+          .setColor(0xf26666)
+          .setTitle('Herkese merhabalar, hoşgeldiniz! 🙌🏻')
+          .setDescription(
+            `Çok kısa sürede kocaman bir ekip olduk, proje yönetiminin sağlıklı bir şekilde yapılabilmesi için yeni gelen arkadaşlarımızın aşağıdaki kayıt formunu doldurması gerekmektedir. Formu doldurduktan sonra ilgili ekiplerimiz başvurunuzu inceleyerek ihtiyaç halinde sizinle iletişime geçeceklerdir. O zaman kadar duyuru kanallarımızdan tüm projelerimiz hakkında bilgi alabilirsin.\n**[📌 Kayıt Formu](https://app.retable.io/form/j3DtfkIkYlqj2cxn)**\n\nFikirlerinizi bizimle paylaşmaya hevesli olduğunuz için çok mutluyuz.Fakat fikirlerinizin incelenmesi ve hiç bir fikrin gözden kaçmaması için aşağıdaki formu doldurmanız gerekmektedir.\n**[📌 Fikir Formu](https://app.retable.io/form/RVC67K3Ai5kQ5w4s)** | **[🗣 Github Discussions](<https://github.com/acikkaynak/deprem-yardim-projesi/discussions>)**\n\nGeliştirilen uygulamalar ve sunucu ile ilgili feedbacklerinizi aşağıdaki formu doldurarak bize iletebilirsiniz.\n **[📌 Feedback Formu](https://app.retable.io/form/NIfo34pdWuweWtfI)**\n\nİlginiz için teşekkürler sizi daha yakından tanımamız için aşağıdaki formu lütfen doldurun. Kurumsal iletişim ekibimiz sizler ile iletişime geçeceklerdir.\n**[📌 Kurumsal Destek Formu](https://app.retable.io/form/qdjgyl2NHrE5CLnA)**`,
+          )
+          .addFields({
+            name: '**Açık Yazılım Ağı**',
+            value:
+              '**[⛓ Github Repoları](https://github.com/orgs/acikkaynak/repositories)\n[⛓ Instagram](https://www.instagram.com/acikyazilimagi/)\n[⛓ Twitter](https://twitter.com/acikyazilimagi)**',
+          })
+          .setTimestamp()
+          .setFooter({ text: 'Her bir saatte bir otomatik paylaşılır.' })
 
-      client.channels.cache.get(mainAnnouncement.channelId).send({ embeds: [announcementEmbed] })
-    }, mainAnnouncement.interval * 1000)
+        client.channels.cache.get(mainAnnouncement.channelId).send({ embeds: [announcementEmbed] })
+      }
+    }, mainAnnouncement.morningInterval * 1000)
+    setInterval(() => {
+      if (new Date().getHours() < 9 || new Date().getHours() > 21) {
+        const announcementEmbed = new EmbedBuilder()
+          .setColor(0xf26666)
+          .setTitle('Herkese merhabalar, hoşgeldiniz! 🙌🏻')
+          .setDescription(
+            `Çok kısa sürede kocaman bir ekip olduk, proje yönetiminin sağlıklı bir şekilde yapılabilmesi için yeni gelen arkadaşlarımızın aşağıdaki kayıt formunu doldurması gerekmektedir. Formu doldurduktan sonra ilgili ekiplerimiz başvurunuzu inceleyerek ihtiyaç halinde sizinle iletişime geçeceklerdir. O zaman kadar duyuru kanallarımızdan tüm projelerimiz hakkında bilgi alabilirsin.\n**[📌 Kayıt Formu](https://app.retable.io/form/j3DtfkIkYlqj2cxn)**\n\nFikirlerinizi bizimle paylaşmaya hevesli olduğunuz için çok mutluyuz.Fakat fikirlerinizin incelenmesi ve hiç bir fikrin gözden kaçmaması için aşağıdaki formu doldurmanız gerekmektedir.\n**[📌 Fikir Formu](https://app.retable.io/form/RVC67K3Ai5kQ5w4s)** | **[🗣 Github Discussions](<https://github.com/acikkaynak/deprem-yardim-projesi/discussions>)**\n\nGeliştirilen uygulamalar ve sunucu ile ilgili feedbacklerinizi aşağıdaki formu doldurarak bize iletebilirsiniz.\n **[📌 Feedback Formu](https://app.retable.io/form/NIfo34pdWuweWtfI)**\n\nİlginiz için teşekkürler sizi daha yakından tanımamız için aşağıdaki formu lütfen doldurun. Kurumsal iletişim ekibimiz sizler ile iletişime geçeceklerdir.\n**[📌 Kurumsal Destek Formu](https://app.retable.io/form/qdjgyl2NHrE5CLnA)**`,
+          )
+          .addFields({
+            name: '**Açık Yazılım Ağı**',
+            value:
+              '**[⛓ Github Repoları](https://github.com/orgs/acikkaynak/repositories)\n[⛓ Instagram](https://www.instagram.com/acikyazilimagi/)\n[⛓ Twitter](https://twitter.com/acikyazilimagi)**',
+          })
+          .setTimestamp()
+          .setFooter({ text: 'Her bir saatte bir otomatik paylaşılır.' })
+
+        client.channels.cache.get(mainAnnouncement.channelId).send({ embeds: [announcementEmbed] })
+      }
+    }, mainAnnouncement.nightInterval * 1000)
     // SAAT BAŞI HATIRLATMA
+    // An interval which will be executed every 1 hour if the time is morning (6:00 - 21:00)
   },
 }
